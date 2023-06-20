3b52ead58e8f3473bf4549c6924fe3ab5413df97
billychen0894 <billychen0894@gmail.com>
2023-06-19 21:42:28 -0700

    feat: add Hero component for homepage

---
+++ components/Homepage/Hero.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import {
+  heroCTALabel,
+  heroSubtitle,
+  heroTitle,
+  heroUrl,
+  heroUrlAlt,
+} from '@const/hero';
+
+export function Hero() {
+  return (
+    <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40 relative isolate overflow-hidden">
+      {/* Background image */}
+      <Image
+        src={heroUrl}
+        alt={heroUrlAlt}
+        className="absolute inset-0 -z-10 h-full w-full object-cover"
+        width={1920}
+        height={1080}
+      />
+      {/* Backdrop */}
+      <div
+        className="absolute inset-0 bg-gray-800 bg-opacity-75 -z-10"
+        aria-hidden="true"
+      />
+      <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8 flex flex-col justify-center items-center">
+        <div className="sm:max-w-lg flex flex-col justify-center items-center text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            {heroTitle}
+          </h1>
+          <p className="mt-4 text-xl text-gray-300">{heroSubtitle}</p>
+        </div>
+        <div>
+          <div className="mt-10">
+            <Link
+              href="/products"
+              className="inline-block rounded-md border border-transparent bg-primary-500 px-8 py-3 text-center font-medium text-white hover:bg-primary-400"
+            >
+              {heroCTALabel}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ const/hero.ts
@@ -0,0 +1,9 @@
+export const heroTitle: string = 'Bossom Lane';
+
+export const heroSubtitle: string =
+  'Discover the enchantment of Blossom Lane, where floral dreams come to life with exquisite arrangements and vibrant blooms';
+
+export const heroCTALabel: string = 'Shop Now';
+
+export const heroUrl: string = '/images/cover1.jpg';
+export const heroUrlAlt: string = 'Blossom Lane';
