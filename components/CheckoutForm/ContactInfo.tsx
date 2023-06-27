e2aa6f4147401250a80c7fb89804b5fbced4e18c
billychen0894 <billychen0894@gmail.com>
2023-06-26 20:40:17 -0700

    refactor: ContactInfo and ShippingInfo title

--- components/CheckoutForm/ContactInfo.tsx
+++ components/CheckoutForm/ContactInfo.tsx
@@ -8,6 +8,7 @@ import {
 } from '@components/ui/form';
 import { UseFormReturn } from 'react-hook-form';
 
+import Link from 'next/link';
 import { type FormData } from './CheckoutForm';
 
 interface ContactInfoProps {
@@ -16,20 +17,33 @@ interface ContactInfoProps {
 
 export default function ContactInfo({ form }: ContactInfoProps) {
   return (
-    <div className="mt-4">
-      <FormField
-        control={form.control}
-        name="contactEmail"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel htmlFor={field.name}>Email address</FormLabel>
-            <FormControl className="mt-1">
-              <Input {...field} type="email" autoComplete="email" />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </div>
+    <>
+      <div className="sm:flex sm:justify-between sm:items-center">
+        <h2 className="text-lg font-medium text-gray-900">
+          Contact information
+        </h2>
+        <span className="text-xs sm:text-sm">
+          Already have an account?{' '}
+          <Link href="/signin" className="text-blue-500 hover:text-blue-400">
+            Sign in
+          </Link>
+        </span>
+      </div>
+      <div className="mt-4">
+        <FormField
+          control={form.control}
+          name="contactEmail"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor={field.name}>Email address</FormLabel>
+              <FormControl className="mt-1">
+                <Input {...field} type="email" autoComplete="email" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </>
   );
 }
