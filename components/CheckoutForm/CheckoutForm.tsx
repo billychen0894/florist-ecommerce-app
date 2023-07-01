3ddc882dd482935848a396d5fa8b812de09e06b2
billychen0894 <billychen0894@gmail.com>
2023-06-30 20:24:55 -0700

    refactor: update payment form validation

--- components/CheckoutForm/CheckoutForm.tsx
+++ components/CheckoutForm/CheckoutForm.tsx
@@ -8,6 +8,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 
 import ContactInfo from './ContactInfo';
 import DeliveryMethod from './DeliveryMethod';
+import Payment from './Payment';
 import ShippingInfo from './ShippingInfo';
 import { FormData, formSchema } from './formValidator';
 
@@ -24,11 +25,6 @@ const products = [
     imageAlt: "Front of men's Basic Tee in black.",
   },
   // More products...
-];
-const paymentMethods = [
-  { id: 'credit-card', title: 'Credit card' },
-  { id: 'paypal', title: 'PayPal' },
-  { id: 'etransfer', title: 'eTransfer' },
 ];
 
 export function CheckoutForm() {
@@ -60,8 +56,7 @@ export function CheckoutForm() {
       // creditCardNumber: '',
       // creditCardName: '',
       // creditCardExpiry: '',
-      // creditCardCvc: '',
-      // notes: '',
+      creditCardCvc: '',
     },
   });
 
@@ -87,117 +82,7 @@ export function CheckoutForm() {
             <DeliveryMethod />
           </div>
 
-          {/* Payment */}
-          <div className="mt-10 border-t border-gray-200 pt-10">
-            <h2 className="text-lg font-medium text-gray-900">Payment</h2>
-
-            <fieldset className="mt-4">
-              <legend className="sr-only">Payment type</legend>
-              <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
-                {paymentMethods.map((paymentMethod, paymentMethodIdx) => (
-                  <div key={paymentMethod.id} className="flex items-center">
-                    {paymentMethodIdx === 0 ? (
-                      <input
-                        id={paymentMethod.id}
-                        name="payment-type"
-                        type="radio"
-                        defaultChecked
-                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                    ) : (
-                      <input
-                        id={paymentMethod.id}
-                        name="payment-type"
-                        type="radio"
-                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                    )}
-
-                    <label
-                      htmlFor={paymentMethod.id}
-                      className="ml-3 block text-sm font-medium text-gray-700"
-                    >
-                      {paymentMethod.title}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </fieldset>
-
-            <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
-              <div className="col-span-4">
-                <label
-                  htmlFor="card-number"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Card number
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    id="card-number"
-                    name="card-number"
-                    autoComplete="cc-number"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-4">
-                <label
-                  htmlFor="name-on-card"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Name on card
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    id="name-on-card"
-                    name="name-on-card"
-                    autoComplete="cc-name"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div className="col-span-3">
-                <label
-                  htmlFor="expiration-date"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Expiration date (MM/YY)
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="expiration-date"
-                    id="expiration-date"
-                    autoComplete="cc-exp"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="cvc"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  CVC
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="cvc"
-                    id="cvc"
-                    autoComplete="csc"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+          <Payment />
         </div>
 
         {/* Order summary */}
---
+++ components/CheckoutForm/Payment.tsx
@@ -0,0 +1,126 @@
+import { ErrorMessage } from '@hookform/error-message';
+import { useFormContext } from 'react-hook-form';
+
+import { Input, Label } from '@components/ui';
+import { paymentMethods } from '@const/paymentMethod';
+
+export default function Payment() {
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useFormContext();
+
+  return (
+    <div className="mt-10 border-t border-gray-200 pt-10">
+      <h2 className="text-lg font-medium text-gray-900">Payment</h2>
+
+      {/* Payment Method Radio */}
+      <fieldset className="mt-4">
+        <legend className="sr-only">Payment type</legend>
+        <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+          {paymentMethods.map((paymentMethod, paymentMethodIdx) => (
+            <div key={paymentMethod.id} className="flex items-center">
+              <input
+                id={paymentMethod.id}
+                type="radio"
+                // defaultChecked
+                value={paymentMethod.id}
+                {...register('paymentMethod')}
+                className="h-4 w-4 border-gray-300 text-primary-400 focus:ring-primary-500"
+              />
+              <label
+                htmlFor={paymentMethod.id}
+                className="ml-3 block text-sm font-medium text-gray-700"
+              >
+                {paymentMethod.title}
+              </label>
+            </div>
+          ))}
+          <ErrorMessage
+            errors={errors}
+            name="paymentMethod"
+            as="p"
+            className="text-sm font-medium text-red-500 mt-1 ml-1"
+          />
+        </div>
+      </fieldset>
+
+      {watch('paymentMethod') === 'creditCard' && (
+        <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
+          <div className="col-span-4">
+            <Label htmlFor="creditCardNumber">Card number</Label>
+            <div className="mt-1">
+              <Input
+                id="creditCardNumber"
+                type="text"
+                autoComplete="cc-number"
+                {...register('creditCardNumber')}
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="creditCardNumber"
+              as="p"
+              className="text-sm font-medium text-red-500 mt-1 ml-1"
+            />
+          </div>
+
+          <div className="col-span-4">
+            <Label htmlFor="creditCardName">Name on card</Label>
+            <div className="mt-1">
+              <Input
+                id="creditCardName"
+                type="text"
+                autoComplete="cc-name"
+                {...register('creditCardName')}
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="creditCardName"
+              as="p"
+              className="text-sm font-medium text-red-500 mt-1 ml-1"
+            />
+          </div>
+
+          <div className="col-span-3">
+            <Label htmlFor="creditCardExpiry">Expiration date (MM/YY)</Label>
+            <div className="mt-1">
+              <Input
+                id="creditCardExpiry"
+                type="text"
+                autoComplete="cc-exp"
+                {...register('creditCardExpiry')}
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="creditCardExpiry"
+              as="p"
+              className="text-sm font-medium text-red-500 mt-1 ml-1"
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="creditCardCvc">CVC</Label>
+            <div className="mt-1">
+              <Input
+                id="creditCardCvc"
+                type="text"
+                autoComplete="csc"
+                {...register('creditCardCvc')}
+              />
+            </div>
+            <ErrorMessage
+              errors={errors}
+              name="creditCardCvc"
+              as="p"
+              className="text-sm font-medium text-red-500 mt-1 ml-1"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
