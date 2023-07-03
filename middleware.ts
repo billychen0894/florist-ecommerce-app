df94f4f0e4160d93005aabcff5a350ae6b7ec49b
billychen0894 <billychen0894@gmail.com>
2023-07-02 20:17:56 -0700

    refactor: update public routes auth

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from '@clerk/nextjs';
 
-export default authMiddleware();
+// Allow public routes for all paths that don't start with /api or /trpc or /user or /admin
+export default authMiddleware({
+  publicRoutes: ['/', '/(products|cart|checkout)(.*)'],
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
