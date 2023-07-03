5f25fd476125085b6a0b9d47b827be65a8dcfd9e
billychen0894 <billychen0894@gmail.com>
2023-07-02 22:02:48 -0700

    fix: update <SignUp/> path url

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <SignUp
-      path="/sign-in"
+      path="/sign-up"
       routing="path"
       signInUrl="/sign-in"
       redirectUrl="/account"
