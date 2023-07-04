0b32873096a786b339834e56846070cbcc4afd6e
billychen0894 <billychen0894@gmail.com>
2023-07-03 21:49:38 -0700

    update: middleware private routes

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/account', '/admin/:path*', '/products'] };
+export const config = { matcher: ['/account', '/admin/:path*'] };
