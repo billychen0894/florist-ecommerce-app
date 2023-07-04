46e9ebd2f24e028391082e233a234ec78e221d2e
billychen0894 <billychen0894@gmail.com>
2023-07-03 21:39:03 -0700

    update: remove @clerk/nextjs related pages

--- components/Provider.tsx
+++
@@ -1,9 +0,0 @@
-import store from '@store';
-import { Provider } from 'react-redux';
-
-// This is a wrapper component for the Redux Provider on client side Components Only
-const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
-};
-
-export default Providers;
