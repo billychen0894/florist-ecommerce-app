c674fe530cd801480640300290ca901b2e46ab45
billychen0894 <billychen0894@gmail.com>
2023-07-05 00:06:14 -0700

    chore: add google hostname to nextConfig image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['tailwindui.com', 'api.unsplash.com', 'images.unsplash.com'],
+    domains: ['tailwindui.com', 'lh3.googleusercontent.com'],
   },
 };
 
