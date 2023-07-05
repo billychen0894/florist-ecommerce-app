3507e3e0d634e5edc23338f151866474cf2293bc
billychen0894 <billychen0894@gmail.com>
2023-07-04 21:43:35 -0700

    feat: implement jwt access token in session

---
+++ lib/jwt.ts
@@ -0,0 +1,50 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+interface SignOptions {
+  expiresIn?: string;
+}
+
+const DEFAULT_SIGN_OPTIONS: SignOptions = {
+  expiresIn: '1d',
+};
+
+export function signJwtAccessToken(
+  payload: JwtPayload,
+  options: SignOptions = DEFAULT_SIGN_OPTIONS
+) {
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret) {
+    throw new Error('Missing JWT_SECRET env variable');
+  }
+
+  if (!payload) {
+    throw new Error('Missing payload');
+  }
+
+  const token = jwt.sign(payload, secret, options);
+
+  return token;
+}
+
+export function verifyJwtAccessToken(token: string) {
+  try {
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) {
+      throw new Error('Missing JWT_SECRET env variable');
+    }
+
+    if (!token) {
+      throw new Error('Missing token');
+    }
+
+    // Decode the token
+    const payload = jwt.verify(token, secret);
+
+    return payload as JwtPayload;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
