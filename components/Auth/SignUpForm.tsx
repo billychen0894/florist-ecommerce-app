7cf76e199159a1859d6aabc61d42573c69965bfa
billychen0894 <billychen0894@gmail.com>
2023-07-13 21:11:50 -0700

    feat: add success/error modal for signUp form submission

--- components/Auth/SignUpForm.tsx
+++ components/Auth/SignUpForm.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { Input, Label } from '@components/ui';
+import { FaceFrownIcon, FaceSmileIcon } from '@heroicons/react/20/solid';
 import { ErrorMessage } from '@hookform/error-message';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { debounce } from 'lodash';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-hot-toast';
 import * as yup from 'yup';
 
+import Modal from '@components/ui/Modal';
 import { asyncCacheTest } from '@lib/asyncCacheTest';
 
 // Override default email regex
@@ -85,6 +88,9 @@ export const signUpFormSchema = yup.object().shape({
 export type SignUpFormData = yup.InferType<typeof signUpFormSchema>;
 
 function SignUpForm() {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [isAccountCreated, setIsAccountCreated] = useState<boolean>(false);
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -98,10 +104,12 @@ function SignUpForm() {
       password: '',
       confirmPassword: '',
     },
+    mode: 'onChange',
   });
 
   const onSubmit = async (data: SignUpFormData) => {
     try {
+      setIsAccountCreated(false);
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`, {
         method: 'POST',
         headers: {
@@ -110,18 +118,23 @@ function SignUpForm() {
         body: JSON.stringify(data),
       });
 
+      // throw error with status code and message in the request response
       if (!res.ok) {
-        toast.error('Something went wrong, please try again later.');
+        setIsAccountCreated(false);
+        setModalOpen(true);
         throw {
           status: res.status,
-          message: 'Something went wrong',
+          message: 'Something went wrong, please try again later',
         };
       }
 
-      // toast.success('You have successfully signed up!');
       const user = await res.json();
+      setIsAccountCreated(true);
+      setModalOpen(true);
       return user;
     } catch (error) {
+      setIsAccountCreated(false);
+      setModalOpen(true);
       console.error(error);
     }
   };
@@ -140,122 +153,156 @@ function SignUpForm() {
   } = register('email');
 
   return (
-    <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <Label htmlFor="firstName" className="leading-6 text-gray-900">
-          First name
-        </Label>
-        <div className="mt-2">
-          <Input
-            id="firstName"
-            type="text"
-            autoComplete="given-name"
-            {...register('firstName')}
-            className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
-          />
-        </div>
-        <ErrorMessage
-          errors={errors}
-          name="firstName"
-          as="p"
-          className="text-sm font-medium text-red-500 mt-1 ml-1"
+    <>
+      {isAccountCreated ? (
+        <Modal
+          open={modalOpen}
+          setOpen={setModalOpen}
+          title="Account registration successful"
+          description="You have successfully registered your account. Please check your email to verify your account."
+          buttonText="Go to homepage"
+          svgIcon={
+            <FaceSmileIcon
+              className="h-6 w-6 text-green-600"
+              aria-hidden="true"
+            />
+          }
+          buttonAction={() => router.push('/')}
         />
-      </div>
-      <div>
-        <Label htmlFor="lastName" className="leading-6 text-gray-900">
-          Last name
-        </Label>
-        <div className="mt-2">
-          <Input
-            id="lastName"
-            type="text"
-            autoComplete="family-name"
-            {...register('lastName')}
-            className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
-          />
-        </div>
-        <ErrorMessage
-          errors={errors}
-          name="lastName"
-          as="p"
-          className="text-sm font-medium text-red-500 mt-1 ml-1"
+      ) : (
+        <Modal
+          open={modalOpen}
+          setOpen={setModalOpen}
+          title="Account registration failed"
+          description="Something went wrong, please try again later."
+          buttonText="Close"
+          svgIcon={
+            <FaceFrownIcon
+              className="h-6 w-6 text-red-600"
+              aria-hidden="true"
+            />
+          }
+          iconBgColor="bg-red-100"
         />
-      </div>
-      <div>
-        <Label htmlFor="email" className="leading-6 text-gray-900">
-          Email address
-        </Label>
-        <div className="mt-2">
-          <Input
-            id="email"
-            type="email"
-            autoComplete="email"
-            name={emailName}
-            ref={emailRef}
-            onChange={debounce(emailOnChange, 1000)}
-            onBlur={debounce(emailOnBlur, 1000)}
-            className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
-          />
-        </div>
-        <ErrorMessage
-          errors={errors}
-          name="email"
-          as="p"
-          className="text-sm font-medium text-red-500 mt-1 ml-1"
-        />
-      </div>
+      )}
 
-      <div>
-        <Label htmlFor="password" className="leading-6 text-gray-900">
-          Password
-        </Label>
-        <div className="mt-2">
-          <Input
-            id="password"
-            type="password"
-            autoComplete="current-password"
-            {...register('password')}
-            className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
+      <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Label htmlFor="firstName" className="leading-6 text-gray-900">
+            First name
+          </Label>
+          <div className="mt-2">
+            <Input
+              id="firstName"
+              type="text"
+              autoComplete="given-name"
+              {...register('firstName')}
+              className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
+            />
+          </div>
+          <ErrorMessage
+            errors={errors}
+            name="firstName"
+            as="p"
+            className="text-sm font-medium text-red-500 mt-1 ml-1"
           />
         </div>
-        <ErrorMessage
-          errors={errors}
-          name="password"
-          as="p"
-          className="text-sm font-medium text-red-500 mt-1 ml-1"
-        />
-      </div>
-
-      <div>
-        <Label htmlFor="confirmPassword" className="leading-6 text-gray-900">
-          Confirm password
-        </Label>
-        <div className="mt-2">
-          <Input
-            id="confirmPassword"
-            type="password"
-            autoComplete="current-password"
-            {...register('confirmPassword')}
-            className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
+        <div>
+          <Label htmlFor="lastName" className="leading-6 text-gray-900">
+            Last name
+          </Label>
+          <div className="mt-2">
+            <Input
+              id="lastName"
+              type="text"
+              autoComplete="family-name"
+              {...register('lastName')}
+              className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
+            />
+          </div>
+          <ErrorMessage
+            errors={errors}
+            name="lastName"
+            as="p"
+            className="text-sm font-medium text-red-500 mt-1 ml-1"
           />
         </div>
-        <ErrorMessage
-          errors={errors}
-          name="confirmPassword"
-          as="p"
-          className="text-sm font-medium text-red-500 mt-1 ml-1"
-        />
-      </div>
+        <div>
+          <Label htmlFor="email" className="leading-6 text-gray-900">
+            Email address
+          </Label>
+          <div className="mt-2">
+            <Input
+              id="email"
+              type="email"
+              autoComplete="email"
+              name={emailName}
+              ref={emailRef}
+              onChange={debounce(emailOnChange, 1000)}
+              onBlur={emailOnBlur}
+              className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
+            />
+          </div>
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            as="p"
+            className="text-sm font-medium text-red-500 mt-1 ml-1"
+          />
+        </div>
 
-      <div>
-        <button
-          type="submit"
-          className="flex w-full justify-center rounded-md bg-primary-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500"
-        >
-          Create Account
-        </button>
-      </div>
-    </form>
+        <div>
+          <Label htmlFor="password" className="leading-6 text-gray-900">
+            Password
+          </Label>
+          <div className="mt-2">
+            <Input
+              id="password"
+              type="password"
+              autoComplete="current-password"
+              {...register('password')}
+              className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
+            />
+          </div>
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            as="p"
+            className="text-sm font-medium text-red-500 mt-1 ml-1"
+          />
+        </div>
+
+        <div>
+          <Label htmlFor="confirmPassword" className="leading-6 text-gray-900">
+            Confirm password
+          </Label>
+          <div className="mt-2">
+            <Input
+              id="confirmPassword"
+              type="password"
+              autoComplete="current-password"
+              {...register('confirmPassword')}
+              className="border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:leading-6"
+            />
+          </div>
+          <ErrorMessage
+            errors={errors}
+            name="confirmPassword"
+            as="p"
+            className="text-sm font-medium text-red-500 mt-1 ml-1"
+          />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-primary-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500"
+          >
+            Create Account
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
