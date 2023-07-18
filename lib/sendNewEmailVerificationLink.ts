2d26bfe22063c6650bf31a1f5ebb116e33471adb
billychen0894 <billychen0894@gmail.com>
2023-07-17 21:48:40 -0700

    feat: add sendNewEmailVerificationLink helper

---
+++ lib/sendNewEmailVerificationLink.ts
@@ -0,0 +1,74 @@
+import { signJwtAccessToken, verifyJwtAccessToken } from './jwt';
+
+export interface sendNewEmailVerificationLinkProps {
+  emailVerificationToken: string | null;
+  setEmailVerificationToken: (token: string) => void;
+  setIsEmailTokenExpired: (isExpired: boolean) => void;
+}
+
+export const sendNewEmailVerificationLink = async ({
+  emailVerificationToken,
+  setEmailVerificationToken,
+  setIsEmailTokenExpired,
+}: sendNewEmailVerificationLinkProps) => {
+  // generate new email verification token
+  try {
+    if (!emailVerificationToken) {
+      return false;
+    }
+
+    const { email, firstName, lastName } = verifyJwtAccessToken(
+      emailVerificationToken
+    ) as { email: string; firstName: string; lastName: string };
+
+    const newEmailVerifyToken = signJwtAccessToken({
+      email,
+      firstName,
+      lastName,
+    });
+
+    setEmailVerificationToken(newEmailVerifyToken);
+
+    // save new email verification token to db
+    const newEmailVerifyTokenResult = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/users`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          emailVerifyToken: newEmailVerifyToken,
+        }),
+      }
+    );
+
+    const newEmailVerifyTokenData = await newEmailVerifyTokenResult.json();
+    console.log('newEmailVerifyTokenData: ', newEmailVerifyTokenData);
+
+    if (newEmailVerifyTokenData?.status === 201) {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/sendEmail`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: email,
+            emailVerificationToken: newEmailVerifyToken,
+            firstName: firstName + 'new',
+          }),
+        });
+
+        setIsEmailTokenExpired(false);
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
