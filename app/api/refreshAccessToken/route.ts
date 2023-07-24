023f388e0f8f70294d91807dda784836769dae88
billychen0894 <billychen0894@gmail.com>
2023-07-23 18:25:08 -0700

    update: add bearer token authorization on every api call

--- app/api/refreshAccessToken/route.ts
+++ app/api/refreshAccessToken/route.ts
@@ -1,9 +1,32 @@
-import { signJwtAccessToken, verifyJwtRefreshToken } from '@lib/jwt';
+import {
+  signJwtAccessToken,
+  verifyJwtAccessToken,
+  verifyJwtRefreshToken,
+} from '@lib/jwt';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request, res: Response) {
   const body: { refreshToken: string } = await req.json();
   const { refreshToken } = body;
+
+  const bearerToken = req.headers.get('authorization')?.split(' ')[1];
+
+  if (!bearerToken) {
+    return NextResponse.json({
+      success: false,
+      status: 401,
+      message: 'Unauthorized',
+    });
+  }
+
+  const accessTokenPayload = verifyJwtAccessToken(bearerToken);
+  if (!accessTokenPayload) {
+    return NextResponse.json({
+      success: false,
+      status: 401,
+      message: 'Unauthorized access token',
+    });
+  }
 
   if (!refreshToken) {
     return NextResponse.json({
