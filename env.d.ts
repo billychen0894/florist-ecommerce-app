ca626242a1bd83a4f71f665cc7f24ae8fc90badc
billychen0894 <billychen0894@gmail.com>
2023-08-01 19:23:59 -0700

    add: type file for .env file to provide better developer experience for code completion and include type safety

---
+++ env.d.ts
@@ -0,0 +1,18 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    APP_BASE_URL: string;
+    NEXTAUTH_URL: string;
+    DATABASE_URL: string;
+    NEXTAUTH_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    JWT_ACCESS_TOKEN_SECRET: string;
+    JWT_REFRESH_TOKEN_SECRET: string;
+    EMAIL_USER_NAME: string;
+    EMAIL_SERVER_USER: string;
+    EMAIL_SERVER_PASSWORD: string;
+    EMAIL_SERVER_HOST: string;
+    EMAIL_SERVER_PORT: string;
+    EMAIL_FROM: string;
+  }
+}
