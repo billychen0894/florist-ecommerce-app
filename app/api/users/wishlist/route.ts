35ed010df04d0027068e2ed1b2a5ae44cb30a0d3
billychen0894 <billychen0894@gmail.com>
2023-08-07 17:45:18 -0700

    add: wishlist POST api route to add products to wishlist

---
+++ app/api/users/wishlist/route.ts
@@ -0,0 +1,182 @@
+import { verifyJwtAccessToken } from '@lib/jwt';
+import { prisma } from '@lib/prisma';
+import { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request, res: Response) {
+  const body: {
+    userId: string;
+    productId: string;
+  } = await req.json();
+
+  const bearerToken = req.headers.get('authorization')?.split(' ')[1];
+  const { userId, productId } = body;
+
+  if (!bearerToken) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Unauthorized',
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const validTokenPayload = verifyJwtAccessToken(bearerToken) as
+    | JwtPayload
+    | Error;
+
+  if (validTokenPayload instanceof TokenExpiredError) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Unauthorized: Token Expired',
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  if (validTokenPayload instanceof Error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Unauthorized',
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  if (validTokenPayload?.role !== 'user') {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Unauthorized',
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  if (validTokenPayload?.id !== userId) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Unauthorized',
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  if (!userId && !productId) {
+    return NextResponse.json(
+      {
+        success: false,
+        data: null,
+        error: 'ValidationError',
+        message: 'Both userId and productId are required',
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  // Check if user exists in db
+  const existingUser = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+  // Check if product exists in db
+  const existingProduct = await prisma.product.findUnique({
+    where: { id: productId },
+  });
+
+  if (!existingUser) {
+    return NextResponse.json(
+      {
+        success: false,
+        data: null,
+        error: 'NotFoundError',
+        message: 'User not found',
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  if (!existingProduct) {
+    return NextResponse.json(
+      {
+        success: false,
+        data: null,
+        error: 'NotFoundError',
+        message: 'Product not found',
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  // Check if product is already in user wishlist
+  const existingWishlistItem = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+    select: {
+      wishlist: {
+        where: {
+          id: productId,
+        },
+      },
+    },
+  });
+
+  if (existingWishlistItem?.wishlist.length) {
+    return NextResponse.json(
+      {
+        success: false,
+        data: null,
+        error: 'ValidationError',
+        message: 'Product already in wishlist',
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  // Add product to user wishlist
+  await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      wishlist: {
+        connect: {
+          id: productId,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(
+    {
+      success: true,
+      data: existingProduct,
+      message: 'Product added to wishlist',
+    },
+    {
+      status: 200,
+    }
+  );
+}
