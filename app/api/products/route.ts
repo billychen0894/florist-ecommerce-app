ef954c6b22704fb307d91cd70eec7460401b60ab
billychen0894 <billychen0894@gmail.com>
2023-08-08 14:20:46 -0700

    update: include error handler in GET products route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,29 +2,42 @@ import { prisma } from '@lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request, res: Response) {
-  const products = await prisma.product.findMany();
+  try {
+    const products = await prisma.product.findMany();
 
-  if (!products) {
+    if (!products) {
+      return NextResponse.json(
+        {
+          success: true,
+          data: [],
+          message: 'No products found',
+        },
+        {
+          status: 200,
+        }
+      );
+    }
+
     return NextResponse.json(
       {
         success: true,
-        data: [],
-        message: 'No products found',
+        data: products,
+        message: 'Products found',
       },
       {
         status: 200,
       }
     );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        data: null,
+        message: 'Something went wrong',
+      },
+      {
+        status: 500,
+      }
+    );
   }
-
-  return NextResponse.json(
-    {
-      success: true,
-      data: products,
-      message: 'Products found',
-    },
-    {
-      status: 200,
-    }
-  );
 }
