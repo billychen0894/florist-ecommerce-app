457e1bd83012fb6e047052a8858f4f3463294773
billychen0894 <billychen0894@gmail.com>
2023-08-12 21:18:06 -0700

    add: api route for creating products and its related method

---
+++ app/api/admin/products/route.ts
@@ -0,0 +1,155 @@
+import { verifyJwtAccessToken } from '@lib/jwt';
+import { prisma } from '@lib/prisma';
+import { ProductPayload } from '@lib/types/api';
+import { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
+import { NextResponse } from 'next/server';
+import { productsPayloadSchema } from './productsPayloadValidation';
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const body: ProductPayload = await req.json();
+    const {
+      name,
+      description,
+      price,
+      images,
+      inStock,
+      leadTime,
+      categories,
+      productDetail,
+    } = body;
+
+    try {
+      await productsPayloadSchema.validate(body, {
+        abortEarly: true,
+      });
+    } catch (error) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: error,
+        },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    const bearerToken = req.headers.get('authorization')?.split(' ')[1];
+
+    if (!bearerToken) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Unauthorized',
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const validTokenPayload = verifyJwtAccessToken(bearerToken) as
+      | JwtPayload
+      | Error;
+
+    if (validTokenPayload instanceof TokenExpiredError) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Unauthorized: Token Expired',
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    if (validTokenPayload instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Unauthorized',
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    if (validTokenPayload?.role !== 'admin') {
+      return NextResponse.json(
+        {
+          success: false,
+          message: `Unauthorized: ${validTokenPayload?.role} role does not have access to this resource.`,
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        images: {
+          createMany: {
+            data: images,
+            skipDuplicates: true,
+          },
+        },
+        inStock,
+        leadTime,
+        categories: {
+          connect: categories.map((category) => ({ name: category.name })),
+        },
+        productDetail: {
+          create: {
+            productDetailItems: {
+              createMany: {
+                data: productDetail.productDetailItems,
+                skipDuplicates: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!product) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Something went wrong during product creation.',
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: 'Product created successfully.',
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        success: false,
+        error: error,
+        message: 'Something went wrong during product creation.',
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- lib/api/admin.ts
+++ lib/api/admin.ts
@@ -11,6 +11,16 @@ async function getAllUsers(
   return response;
 }
 
+async function createProduct(
+  axiosWithAuth: AxiosInstance
+): Promise<ApiResponse<null>> {
+  const response = (await axiosWithAuth.post(
+    '/api/admin/products'
+  )) as ApiResponse<null>;
+  return response;
+}
+
 export const admin = {
   getAllUsers,
+  createProduct,
 };
