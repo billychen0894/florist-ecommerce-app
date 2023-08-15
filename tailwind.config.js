f03841ca6e8d19dc1d0c3f500c80921f5160f440
billychen0894 <billychen0894@gmail.com>
2023-08-15 14:59:09 -0700

    chore: update clsx and tailwind-merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,10 +31,10 @@ module.exports = {
         },
       },
     },
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+      require('@tailwindcss/typography'),
+    ],
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ],
 };
