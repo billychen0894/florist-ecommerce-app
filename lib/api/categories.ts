720d7b5329fe9c27a2de63444e3c721d923682ba
billychen0894 <billychen0894@gmail.com>
2023-08-21 22:17:08 -0700

    update: getAllCategories method

--- lib/api/categories.ts
+++ lib/api/categories.ts
@@ -1,11 +1,14 @@
 import axios from '@lib/axios';
 import { ApiResponse, Categories } from '@lib/types/api';
+import { AxiosResponse } from 'axios';
 
-async function getAllCategories(): Promise<ApiResponse<Categories[]>> {
-  const response = (await axios.get(`/api/categories`)) as ApiResponse<
-    Categories[]
+async function getAllCategories(): Promise<
+  AxiosResponse<ApiResponse<Categories[]>>
+> {
+  const response = (await axios.get(`/api/categories`)) as AxiosResponse<
+    ApiResponse<Categories[]>
   >;
-  return response;
+  return response as AxiosResponse<ApiResponse<Categories[]>>;
 }
 
 export const categories = {
