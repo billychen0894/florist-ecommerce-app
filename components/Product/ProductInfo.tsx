48621cc101b6a7400841afdee10ca1860e1ef074
billychen0894 <billychen0894@gmail.com>
2023-08-24 22:30:07 -0700

    update: product props type and adding category view

--- components/Product/ProductInfo.tsx
+++ components/Product/ProductInfo.tsx
@@ -1,26 +1,31 @@
-import { Product } from '@const/products';
+import { Product, ProductPayload } from '@lib/types/api';
 
 interface ProductInfoProps {
-  product: Product;
+  product: Product | null;
 }
 
 export function ProductInfo({ product }: ProductInfoProps) {
   return (
     <>
       <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-        {product.name}
+        {product?.name}
       </h1>
-      <div className="mt-3">
+      <div className="mt-3 space-y-3">
         <h2 className="sr-only">Product information</h2>
+        <p className="text-sm tracking-tight text-shades-400">
+          {(product as ProductPayload)?.categories
+            .map((category) => category.name)
+            .join(', ')}
+        </p>
         <p className="text-3xl tracking-tight text-secondary-500">
-          ${product.price}
+          ${product?.price}
         </p>
       </div>
 
       <div className="mt-6">
         <h3 className="sr-only">Description</h3>
         <span className="space-y-6 text-base text-shades-500">
-          {product.description}
+          {product?.description}
         </span>
       </div>
     </>
