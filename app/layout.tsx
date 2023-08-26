7e882977a34854672d0bf6b5d64e29c97cc98deb
billychen0894 <billychen0894@gmail.com>
2023-08-25 23:37:22 -0700

    update: add Redux Provider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
 
 import { Footer } from '@components/Footer';
 import { Navigation } from '@components/Navigation';
 import AuthProvider from '@components/Providers/AuthProvider';
+import ReduxProvider from '@store/ReduxProvider';
 import '@styles/globals.css';
-import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'], weight: ['400', '600', '700'] });
 
@@ -20,23 +21,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <AuthProvider>
-      <html lang="en" className="h-full">
-        <body className={`${inter.className} h-full`}>
-          <Toaster
-            toastOptions={{
-              style: {
-                color: '#111827',
-                fontSize: '0.8rem',
-                backgroundColor: '#F9FAFB',
-              },
-            }}
-          />
-          <Navigation />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </AuthProvider>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} h-full`}>
+        <ReduxProvider>
+          <AuthProvider>
+            <Toaster
+              toastOptions={{
+                style: {
+                  color: '#111827',
+                  fontSize: '0.8rem',
+                  backgroundColor: '#F9FAFB',
+                },
+              }}
+            />
+            <Navigation />
+            {children}
+            <Footer />
+          </AuthProvider>
+        </ReduxProvider>
+      </body>
+    </html>
   );
 }
