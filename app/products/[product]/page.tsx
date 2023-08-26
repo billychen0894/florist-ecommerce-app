a5db50b47a495103795ed6c7cc64171f99764d80
billychen0894 <billychen0894@gmail.com>
2023-08-26 15:48:22 -0700

    update: add ProductActions component to [Product] page

--- app/products/[product]/page.tsx
+++ app/products/[product]/page.tsx
@@ -1,12 +1,10 @@
-import { HeartIcon } from '@heroicons/react/24/outline';
-
 import {
+  ProductActions,
   ProductDetails,
   ProductImageGallery,
   ProductInfo,
   ProductsRecommendation,
 } from '@components/Product';
-import Button from '@components/ui/Button';
 import { products } from '@lib/api/products';
 
 export default async function Product({
@@ -16,6 +14,7 @@ export default async function Product({
 }) {
   const productResult = await products.getProductById(params.product);
   const product = productResult.data.data;
+
   return (
     <div className="bg-white">
       <main className="mx-auto max-w-7xl sm:px-6 sm:pt-16 lg:px-8">
@@ -28,47 +27,7 @@ export default async function Product({
             {/* Product info */}
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               <ProductInfo product={product} />
-
-              <form className="mt-6">
-                <div className="mt-10 flex">
-                  <div className="flex mr-4">
-                    <label
-                      htmlFor={`quantity-${params.product}`}
-                      className="sr-only"
-                    >
-                      Quantity, {product?.name}
-                    </label>
-                    <select
-                      id={`quantity-${params.product}`}
-                      name={`quantity-${params.product}`}
-                      className="max-w-full rounded-md border border-gray-300 py-1.5 text-center text-base font-medium leading-5 text-gray-700 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-1 focus:ring-primary-500 sm:text-sm"
-                    >
-                      {[...Array(10)].map((_, i) => (
-                        <option key={i} value={i + 1}>
-                          {i + 1}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <Button
-                    type="submit"
-                    className="flex max-w-xs flex-1 items-center justify-center border border-transparent px-8 py-3 text-base font-medium focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
-                  >
-                    Add to cart
-                  </Button>
-
-                  <Button
-                    type="button"
-                    className="ml-4 flex items-center justify-center px-3 py-3 text-secondary-400 bg-transparent shadow-none hover:bg-gray-100 hover:text-secondary-500"
-                  >
-                    <HeartIcon
-                      className="h-6 w-6 flex-shrink-0"
-                      aria-hidden="true"
-                    />
-                    <span className="sr-only">Add to favorites</span>
-                  </Button>
-                </div>
-              </form>
+              <ProductActions productId={params.product} product={product} />
               <ProductDetails product={product} />
             </div>
           </div>
