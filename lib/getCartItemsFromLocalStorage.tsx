0af3cd51bf6da75a97f9ba4d11899a5908d6d889
billychen0894 <billychen0894@gmail.com>
2023-08-26 16:59:37 -0700

    add: helper to get cached cartItems from localstorage

---
+++ lib/getCartItemsFromLocalStorage.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { OrderItem } from './types/api';
+
+export function getCartItemsFromLocalStorage(): OrderItem[] {
+  const cartItemsStr = localStorage.getItem('cartItems');
+
+  if (cartItemsStr) {
+    const cartItem: OrderItem[] = JSON.parse(cartItemsStr);
+    return cartItem;
+  }
+  return [];
+}
