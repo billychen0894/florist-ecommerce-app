3cc84c58424e330987d436bd10c5bbf02d132b91
billychen0894 <billychen0894@gmail.com>
2023-09-03 21:44:16 -0700

    add: implement subtotal state when adding/removing cart items

--- store/features/cartSlice.ts
+++ store/features/cartSlice.ts
@@ -10,34 +10,45 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export interface CartState {
   cartItems: Record<string, TCartItem>;
+  subtotal: number;
 }
 
 const initialState: CartState = {
   cartItems: {},
+  subtotal: 0,
 };
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    initializeCart(state, action: PayloadAction<TCartItem[]>) {
-      // Convert the array of items to a record for easier access
-      state.cartItems = action.payload.reduce((acc, item) => {
-        acc[item.id] = item;
-        return acc;
-      }, {} as Record<string, TCartItem>);
+    initializeCart(state, action: PayloadAction<Record<string, TCartItem>>) {
+      state.cartItems = action.payload;
+      const cartItemsArr = Object.values(state.cartItems);
+      const subtotal = cartItemsArr.reduce((acc, item) => {
+        return acc + item.quantity * item.product.price;
+      }, 0);
+      state.subtotal = subtotal;
     },
     addItemToCart(state, action: PayloadAction<TCartItem>) {
       const orderItem = action.payload;
       // If the item is already in the cart, update its quantity
       if (state.cartItems[orderItem.id]) {
         state.cartItems[orderItem.id].quantity += orderItem.quantity;
+        state.subtotal += orderItem.quantity * orderItem.product.price;
       } else {
         state.cartItems[orderItem.id] = orderItem;
+        state.subtotal += orderItem.quantity * orderItem.product.price;
       }
     },
     removeItemFromCart(state, action: PayloadAction<{ itemId: string }>) {
       if (state.cartItems.hasOwnProperty(action.payload.itemId)) {
+        const { quantity, product } = state.cartItems[action.payload.itemId];
+        if (state.subtotal - quantity * product.price < 0) {
+          state.subtotal = 0;
+        } else {
+          state.subtotal -= quantity * product.price;
+        }
         delete state.cartItems[action.payload.itemId];
       }
     },
@@ -48,7 +59,12 @@ const cartSlice = createSlice({
       const { itemId, quantity } = action.payload;
       // Update the quantity of a specific item
       if (state.cartItems[itemId]) {
+        const prevSelectedQuantity = state.cartItems[itemId].quantity;
+        const selectedProductPrice = state.cartItems[itemId].product.price;
         state.cartItems[itemId].quantity = quantity;
+        state.subtotal +=
+          quantity * selectedProductPrice -
+          prevSelectedQuantity * selectedProductPrice;
       }
     },
   },
