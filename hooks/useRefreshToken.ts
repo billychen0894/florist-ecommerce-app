1d03ce6e3dfe99727137dfe486c9065a56a87dad
billychen0894 <billychen0894@gmail.com>
2023-09-03 18:12:00 -0700

    fix: when refreshToken is executed, it should also call update() to update accessToken from session object

--- hooks/useRefreshToken.ts
+++ hooks/useRefreshToken.ts
@@ -5,12 +5,15 @@ import { useSession } from 'next-auth/react';
 import { auth } from '@lib/api/auth';
 
 export default function useRefreshToken() {
-  const { data: session } = useSession();
+  const { data: session, update } = useSession();
 
   const refreshToken = async () => {
     const response = await auth.refreshAccessToken(session?.user.refreshToken);
 
-    if (session) session.user.accessToken = response?.data.accessToken;
+    if (session) {
+      session.user.accessToken = response?.data.accessToken;
+      update({ accessToken: response?.data.accessToken });
+    }
   };
 
   return refreshToken;
