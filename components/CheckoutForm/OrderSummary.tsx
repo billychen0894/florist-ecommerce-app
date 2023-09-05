8248d7990169dbea191f37a59027e17b4a110bbd
billychen0894 <billychen0894@gmail.com>
2023-09-04 21:03:15 -0700

    update: makes checkout summary reflecting data from Redux store

--- components/CheckoutForm/OrderSummary.tsx
+++ components/CheckoutForm/OrderSummary.tsx
@@ -1,15 +1,34 @@
+'use client';
+
 import { TrashIcon } from '@heroicons/react/20/solid';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { HoverCard } from '@components/ui';
 import Button from '@components/ui/Button';
-import { Product } from '@const/products';
+import { shippingHoverCardInfo, taxHoverCardInfo } from '@const/orderInfo';
+import { formatCurrency } from '@lib/formatCurrency';
+import {
+  removeItemFromCart,
+  updateCartItemQuantity,
+} from '@store/features/cartSlice';
+import { useAppDispatch, useAppSelector } from '@store/hooks';
+import { ChangeEvent } from 'react';
 
-interface OrderSummaryProps {
-  products: Product[];
-}
+export default function OrderSummary() {
+  const cartItems = useAppSelector((state) => state.cartReducer.cartItems);
+  const subtotal = useAppSelector((state) => state.cartReducer.subtotal);
+  const dispatch = useAppDispatch();
+  const cartItemsArr = Object.values(cartItems);
 
-export default function OrderSummary({ products }: OrderSummaryProps) {
+  const handleRemoveItemFromCart = (itemId: string) => {
+    dispatch(removeItemFromCart({ itemId }));
+  };
+
+  const handleCartItemQuantity = (itemId: string, quantity: number) => {
+    dispatch(updateCartItemQuantity({ itemId, quantity }));
+  };
+
   return (
     <div className="mt-10 lg:mt-0">
       <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
@@ -17,12 +36,12 @@ export default function OrderSummary({ products }: OrderSummaryProps) {
       <div className="mt-4 rounded-lg border border-gray-200 bg-white shadow-sm">
         <h3 className="sr-only">Items in your cart</h3>
         <ul role="list" className="divide-y divide-gray-200">
-          {products.map((product) => (
-            <li key={product.id} className="flex px-4 py-6 sm:px-6">
+          {cartItemsArr.map((cartItem) => (
+            <li key={cartItem.id} className="flex px-4 py-6 sm:px-6">
               <div className="flex-shrink-0">
                 <Image
-                  src={product.images[0].imageUrl}
-                  alt={product.images[0].alt || product.images[0].name}
+                  src={cartItem.product.images[0].url}
+                  alt={cartItem.product.images[0].alt}
                   className="w-20 rounded-md"
                   width={80}
                   height={80}
@@ -34,15 +53,16 @@ export default function OrderSummary({ products }: OrderSummaryProps) {
                   <div className="min-w-0 flex-1">
                     <h4 className="text-sm">
                       <Link
-                        // TODO: Add product page link
-                        href="#"
-                        className="font-medium text-gray-700 hover:text-gray-800"
+                        href={`/products/${cartItem.product.id}`}
+                        className="font-medium text-gray-800 hover:text-gray-500"
                       >
-                        {product.name}
+                        {cartItem.product.name}
                       </Link>
                     </h4>
                     <p className="mt-1 text-sm text-shades-500">
-                      {product.category}
+                      {cartItem.product.categories
+                        .map((category) => category.name)
+                        .join(' ,')}
                     </p>
                   </div>
 
@@ -50,6 +70,9 @@ export default function OrderSummary({ products }: OrderSummaryProps) {
                     <Button
                       type="button"
                       className="-m-2.5 flex items-center justify-center bg-white p-2.5 text-gray-400 hover:text-gray-500 hover:bg-transparent shadow-none"
+                      onClick={() => {
+                        handleRemoveItemFromCart(cartItem.id);
+                      }}
                     >
                       <span className="sr-only">Remove</span>
                       <TrashIcon className="h-5 w-5" aria-hidden="true" />
@@ -59,7 +82,7 @@ export default function OrderSummary({ products }: OrderSummaryProps) {
 
                 <div className="flex flex-1 items-end justify-between pt-2">
                   <p className="mt-1 text-sm font-medium text-secondary-500">
-                    ${product.price}
+                    {formatCurrency(cartItem.product.price, 'en-CA', 'CAD')}
                   </p>
 
                   <div className="ml-4">
@@ -70,17 +93,20 @@ export default function OrderSummary({ products }: OrderSummaryProps) {
                       id="quantity"
                       name="quantity"
                       className="rounded-md border border-gray-300 text-left text-base font-medium text-gray-700 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-1 focus:ring-primary-500 sm:text-sm"
+                      value={cartItem.quantity}
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                        const selectedQuantity = e.target.value;
+                        handleCartItemQuantity(
+                          cartItem.id,
+                          Number(selectedQuantity)
+                        );
+                      }}
                     >
-                      <option value={1}>1</option>
-                      <option value={2}>2</option>
-                      <option value={3}>3</option>
-                      <option value={4}>4</option>
-                      <option value={5}>5</option>
-                      <option value={6}>6</option>
-                      <option value={7}>7</option>
-                      <option value={8}>8</option>
-                      <option value={9}>9</option>
-                      <option value={10}>10</option>
+                      {[...Array(10)].map((_, i) => (
+                        <option key={i} value={i + 1}>
+                          {i + 1}
+                        </option>
+                      ))}
                     </select>
                   </div>
                 </div>
@@ -92,14 +118,28 @@ export default function OrderSummary({ products }: OrderSummaryProps) {
         <dl className="space-y-6 border-t border-gray-200 px-4 py-6 sm:px-6">
           <div className="flex items-center justify-between">
             <dt className="text-sm">Subtotal</dt>
-            <dd className="text-sm font-medium text-gray-900">$64.00</dd>
+            <dd className="text-sm font-medium text-gray-900">
+              {formatCurrency(subtotal, 'en-CA', 'CAD')}
+            </dd>
           </div>
           <div className="flex items-center justify-between">
-            <dt className="text-sm">Shipping</dt>
+            <dt className="flex text-sm">
+              <span>Shipping</span>
+              <HoverCard
+                screenReaderText={shippingHoverCardInfo.screenReaderText}
+                hoverCardText={shippingHoverCardInfo.hoverCardText}
+              />
+            </dt>
             <dd className="text-sm font-medium text-gray-900">$5.00</dd>
           </div>
           <div className="flex items-center justify-between">
-            <dt className="text-sm">Taxes</dt>
+            <dt className="flex text-sm">
+              <span>Taxes</span>
+              <HoverCard
+                screenReaderText={taxHoverCardInfo.screenReaderText}
+                hoverCardText={taxHoverCardInfo.hoverCardText}
+              />
+            </dt>
             <dd className="text-sm font-medium text-gray-900">$5.52</dd>
           </div>
           <div className="flex items-center justify-between border-t border-gray-200 pt-6">
