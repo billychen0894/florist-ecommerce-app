c22dde122aab7e3bdf203aa3f19ce9aab6f0ad56
billychen0894 <billychen0894@gmail.com>
2023-09-13 22:55:03 -0700

    add: fetchUserByStripeId thunk

--- store/features/userSlice.ts
+++ store/features/userSlice.ts
@@ -1,8 +1,10 @@
 import { users } from '@lib/api/users';
+import { stripe } from '@lib/stripe';
 import { User } from '@prisma/client';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { AxiosInstance } from 'axios';
+import Stripe from 'stripe';
 import { TProduct } from './../../lib/types/api.d';
 
 type UserWithoutPass = Omit<User, 'password'>;
@@ -10,11 +12,13 @@ type UserWithoutPass = Omit<User, 'password'>;
 export interface UserState {
   wishlist: TProduct[];
   user: UserWithoutPass | null;
+  userStripe: Stripe.Customer | null;
 }
 
 const initialState: UserState = {
   wishlist: [],
   user: null,
+  userStripe: null,
 };
 
 export const fetchUserWishlistById = createAsyncThunk(
@@ -44,8 +48,22 @@ export const fetchUserById = createAsyncThunk(
   ) => {
     const { userId, axiosWithAuth } = data;
     const response = await users.getUser(userId, axiosWithAuth);
-    const user = response.data;
+    const user = response.data.data;
     return user;
+  }
+);
+
+export const fetchUserByStripeId = createAsyncThunk(
+  'user/fetchUserByStripeId',
+  async (customerStripeId: string | null | undefined, thunkApi) => {
+    if (customerStripeId) {
+      const customer = await stripe.customers.retrieve(customerStripeId, {
+        apiKey: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
+      });
+      return customer as Stripe.Customer;
+    } else {
+      return null;
+    }
   }
 );
 
@@ -68,7 +86,14 @@ const userSlice = createSlice({
         state.wishlist = action.payload as TProduct[];
       })
       .addCase(fetchUserById.fulfilled, (state, action) => {
-        state.user = action.payload;
+        if (action.payload) {
+          state.user = action.payload;
+        }
+      })
+      .addCase(fetchUserByStripeId.fulfilled, (state, action) => {
+        if (action.payload) {
+          state.userStripe = action.payload;
+        }
       });
   },
 });
