b9d8796767de18f5c2fdb34ecf3bb43e158c7235
billychen0894 <billychen0894@gmail.com>
2023-09-17 00:58:08 -0700

    update: RowData props

--- components/Table/RowData.tsx
+++ components/Table/RowData.tsx
@@ -1,17 +1,20 @@
 import { cn } from '@lib/classNames';
-import { Order } from '@prisma/client';
 
 interface RowDataProps {
   rowIndex: number;
-  orders: Order[];
+  invoiceLength: number;
   children: React.ReactNode;
 }
 
-export default function RowData({ rowIndex, orders, children }: RowDataProps) {
+export default function RowData({
+  rowIndex,
+  invoiceLength,
+  children,
+}: RowDataProps) {
   return (
     <td
       className={cn(
-        rowIndex !== orders.length - 1 ? 'border-b border-gray-200' : '',
+        rowIndex !== invoiceLength - 1 ? 'border-b border-gray-200' : '',
         'whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8'
       )}
     >
