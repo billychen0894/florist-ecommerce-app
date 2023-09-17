5c46699f3d32e5ec1970e4f46a7f9f8a20902ae0
billychen0894 <billychen0894@gmail.com>
2023-09-17 00:59:03 -0700

    update: passing invoice data props from Redux

--- components/Table/Row.tsx
+++ components/Table/Row.tsx
@@ -1,36 +1,50 @@
-import { Order } from '@prisma/client';
+import { formatCurrency } from '@lib/formatCurrency';
+import { $Enums } from '@prisma/client';
 import Link from 'next/link';
+import Stripe from 'stripe';
 import RowData from './RowData';
 
 interface RowProps {
   rowIndex: number;
-  orders: Order[];
-  invoiceHref: string;
+  invoice: Stripe.Invoice & { orderStatus: $Enums.OrderStatus };
+  invoicesLength: number;
 }
-export default function Row({ rowIndex, orders, invoiceHref }: RowProps) {
+export default function Row({ rowIndex, invoice, invoicesLength }: RowProps) {
   return (
     <tr>
-      <RowData rowIndex={rowIndex} orders={orders}>
-        test
+      <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
+        {invoice.number}
       </RowData>
-      <RowData rowIndex={rowIndex} orders={orders}>
-        test
+      <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
+        {formatCurrency(Number(invoice.total / 100), 'en-CA', 'CAD')}
       </RowData>
-      <RowData rowIndex={rowIndex} orders={orders}>
-        test
+      <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
+        {invoice.orderStatus ? (
+          <span className="inline-flex items-center rounded-md bg-orange-50 px-2 py-1 text-xs font-medium text-orange-500 ring-1 ring-inset ring-orange-600/20">
+            {invoice.orderStatus.charAt(0) +
+              invoice.orderStatus.slice(1).toLowerCase()}
+          </span>
+        ) : (
+          <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">
+            Error: contact support
+          </span>
+        )}
       </RowData>
-      <RowData rowIndex={rowIndex} orders={orders}>
-        test
+      <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
+        <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+          {invoice.status === 'paid' ? 'Paid' : 'Unpaid'}
+        </span>
       </RowData>
-      <RowData rowIndex={rowIndex} orders={orders}>
-        test
-      </RowData>
-      <RowData rowIndex={rowIndex} orders={orders}>
+      <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
         <Link
-          href={invoiceHref}
+          href={
+            typeof invoice.hosted_invoice_url === 'string'
+              ? invoice.hosted_invoice_url
+              : '#'
+          }
           className="text-secondary-500 hover:text-secondary-300"
         >
-          View Invoice<span className="sr-only">, {''}</span>
+          View invoice<span className="sr-only">, {''}</span>
         </Link>
       </RowData>
     </tr>
