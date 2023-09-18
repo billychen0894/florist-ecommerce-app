53d8525d89e1c82e4b6a27964964985d3b21228d
billychen0894 <billychen0894@gmail.com>
2023-09-17 17:47:49 -0700

    add: import SkeletonTable component for table loading state

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Row from '@components/Table/Row';
+import SkeletonTable from '@components/Table/SkeletonTable';
 import StickyHeader from '@components/Table/StickyHeader';
 import { Order } from '@lib/types/api';
 import { useAppSelector } from '@store/hooks';
@@ -21,32 +22,40 @@ export default function Orders() {
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
-      <div className="mt-8 flow-root">
+      <div className="my-10 flow-root">
         <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle">
-            <table className="min-w-full border-separate border-spacing-0">
-              <thead>
-                <tr>
-                  <StickyHeader>Invoice Number</StickyHeader>
-                  <StickyHeader>Total</StickyHeader>
-                  <StickyHeader>Order Status</StickyHeader>
-                  <StickyHeader>Payment Status</StickyHeader>
-                  <StickyHeader>
-                    <span className="sr-only">View Invoice Details</span>
-                  </StickyHeader>
-                </tr>
-              </thead>
-              <tbody>
-                {invoicesAndOrdersCombined?.map((invoice, idx) => (
-                  <Row
-                    key={invoice.id}
-                    rowIndex={idx}
-                    invoice={invoice}
-                    invoicesLength={invoicesAndOrdersCombined?.length}
-                  />
-                ))}
-              </tbody>
-            </table>
+            {invoicesAndOrdersCombined ? (
+              <table className="min-w-full border-separate border-spacing-0">
+                <thead>
+                  <tr>
+                    <StickyHeader>Invoice Number</StickyHeader>
+                    <StickyHeader className="hidden sm:table-cell">
+                      Total
+                    </StickyHeader>
+                    <StickyHeader className="hidden lg:table-cell">
+                      Order Status
+                    </StickyHeader>
+                    <StickyHeader>Payment Status</StickyHeader>
+                    <StickyHeader>
+                      <span className="sr-only">View Invoice Details</span>
+                    </StickyHeader>
+                  </tr>
+                </thead>
+                <tbody>
+                  {invoicesAndOrdersCombined?.map((invoice, idx) => (
+                    <Row
+                      key={invoice.id}
+                      rowIndex={idx}
+                      invoice={invoice}
+                      invoicesLength={invoicesAndOrdersCombined?.length}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <SkeletonTable />
+            )}
           </div>
         </div>
       </div>
