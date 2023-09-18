17d5d459627e62046e54a2457a0fe7f87db27748
billychen0894 <billychen0894@gmail.com>
2023-09-17 17:47:05 -0700

    add: Skeleton UI component for table

---
+++ components/Table/SkeletonTable.tsx
@@ -0,0 +1,52 @@
+export default function SkeletonTable() {
+  return (
+    <table className="min-w-full border-separate border-spacing-2">
+      <thead>
+        <tr className="animate-pulse">
+          <th className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <th className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <th className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <th className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <th className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="animate-pulse">
+          <td className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+        </tr>
+        <tr className="animate-pulse">
+          <td className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+        </tr>
+        <tr className="animate-pulse">
+          <td className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+        </tr>
+        <tr className="animate-pulse">
+          <td className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+        </tr>
+        <tr className="animate-pulse">
+          <td className="h-8 w-2 my-1 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+          <td className="h-8 w-2 rounded-lg shadow-sm bg-slate-200" />
+        </tr>
+      </tbody>
+    </table>
+  );
+}
