05c76ee727de2409d5aa38f46bce377386f1775c
billychen0894 <billychen0894@gmail.com>
2023-09-19 20:45:23 -0700

    add: personalInfoFormValidator

---
+++ components/User/personalInfoFormValidator.ts
@@ -0,0 +1,68 @@
+import * as yup from 'yup';
+
+// Define a custom validation function for file type
+const validateFileType = (file: any) => {
+  if (!file) return true; // If no file is selected, it's valid (optional)
+
+  const allowedFileTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+  return allowedFileTypes.includes(file.type);
+};
+
+// Define a custom validation function for file size (1MB = 1024 * 1024 bytes)
+const validateFileSize = (file: any) => {
+  if (!file) return true; // If no file is selected, it's valid (optional)
+
+  const maxSizeBytes = 1024 * 1024; // 1MB
+
+  return file.size <= maxSizeBytes;
+};
+
+export interface DefaultPersonalInfoFormSchemaProps {
+  firstName: string;
+  lastName: string;
+  email: string;
+  contactPhone: string;
+  imageFile?: {} | null | undefined;
+}
+
+export const defaultPersonalInfoFormSchema: yup.ObjectSchema<DefaultPersonalInfoFormSchemaProps> =
+  yup.object({
+    firstName: yup
+      .string()
+      .required('First name is required')
+      .max(50, 'First name cannot exceed 50 characters.'),
+    lastName: yup
+      .string()
+      .required('Last name is required')
+      .max(50, 'Last name cannot exceed 50 characters.'),
+    email: yup
+      .string()
+      .email('Your email is invalid')
+      .required('Your email is required.'),
+    contactPhone: yup
+      .string()
+      .required('Please enter your phone number')
+      .max(20, 'Your phone number cannot exceed 20 characters.')
+      .matches(
+        /^(\+?1[ -]?)?\(?([0-9]{3})\)?[ -]?([0-9]{3})[ -]?([0-9]{4})$/,
+        'Your phone number is invalid'
+      ),
+    imageFile: yup
+      .mixed()
+      .test(
+        'fileType',
+        'Invalid file type. Only JPEG, PNG, and GIF are allowed.',
+        validateFileType
+      )
+      .test(
+        'fileSize',
+        'File size exceeds the maximum limit of 1MB.',
+        validateFileSize
+      )
+      .nullable(),
+  });
+
+export type PersonalInfoFormSchema = yup.InferType<
+  typeof defaultPersonalInfoFormSchema
+>;
