5287c949c9f8f751759bf3e7ae82559c37390ea1
billychen0894 <billychen0894@gmail.com>
2023-09-30 21:52:39 -0700

    add: server action for updating new password with user email

---
+++ actions/updateUserByEmail.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import { prisma } from '@lib/prisma';
+import * as bcrypt from 'bcrypt';
+
+export const updateUserPasswordByEmail = async (
+  email: string,
+  newPassword: string
+) => {
+  try {
+    if (!email || !newPassword) {
+      throw new Error('Email and new password are required');
+    }
+
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    const user = await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        password: hashedNewPassword,
+        passwordVerificationToken: '',
+      },
+    });
+
+    if (!user) {
+      throw new Error('Something went wrong during updating user');
+    }
+
+    return true;
+  } catch (err: any) {
+    console.log('Error:', err.message);
+    return false;
+  }
+};
