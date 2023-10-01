09e12ab08573c5dfa994e3213c3c4d8debf152ca
billychen0894 <billychen0894@gmail.com>
2023-09-30 21:52:13 -0700

    add: server action to validate reset password token

---
+++ actions/validateResetPasswordToken.ts
@@ -0,0 +1,39 @@
+'use server';
+import { prisma } from '@lib/prisma';
+import { verifyJwtAccessToken } from '@lib/jwt';
+import { JwtPayload } from 'jsonwebtoken';
+
+export const validateTokenWithUserEmail = async (
+  email: string,
+  token: string
+) => {
+  try {
+    if (!email || !token) {
+      throw new Error('Email and token must be supplied');
+    }
+
+    const payload = verifyJwtAccessToken(token) as JwtPayload;
+    if (email !== payload.email) {
+      throw new Error('Token validation failed');
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('No users found with this email address');
+    }
+
+    if (user.passwordVerificationToken !== token) {
+      throw new Error('Invalid token');
+    }
+
+    return true;
+  } catch (err: any) {
+    console.log('Error:', err.message);
+    return false;
+  }
+};
