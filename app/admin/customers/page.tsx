332bd4276997fd266134f5ea5c8869340773665a
billychen0894 <billychen0894@gmail.com>
2023-10-04 20:48:40 -0700

    add: admin pages

---
+++ app/admin/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Customers() {
+  return (
+    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">Customers</div>
+  );
+}
---
+++ app/admin/settings/page.tsx
@@ -0,0 +1,3 @@
+export default function Settings() {
+  return <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">Settings</div>;
+}
