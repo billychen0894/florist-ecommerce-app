d50e5ba61fec6e7522c4db83759dcce77b93f1c7
billychen0894 <billychen0894@gmail.com>
2023-10-04 20:48:05 -0700

    add: layout page for admin pages

---
+++ app/admin/layout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import AdminNavigation from '@components/Admin/AdminNavigation';
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-full">
+      <AdminNavigation />
+      <main>{children}</main>
+    </div>
+  );
+}
