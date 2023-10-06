dc38ac955446cb042b52552efef1b0ebd9d0de07
billychen0894 <billychen0894@gmail.com>
2023-10-05 20:29:24 -0700

    update: fetching user info when it's on admin pages. Also, adds page authorization

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,33 @@
 'use client';
 
 import AdminNavigation from '@components/Admin/AdminNavigation';
+import { useAppDispatch, useAppSelector } from '@store/hooks';
+import { useEffect } from 'react';
+import { fetchUserById } from '@store/features/userSlice';
+import { useSession } from 'next-auth/react';
+import useAxiosWithAuth from '@hooks/useAxiosAuth';
+import { redirect } from 'next/navigation';
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const admin = useAppSelector((state) => state.userReducer.user);
+  const dispatch = useAppDispatch();
+  const axiosWithAuth = useAxiosWithAuth();
+  const { data: session, status } = useSession();
+
+  if (session?.user.role !== 'admin' && status === 'unauthenticated') {
+    redirect('/denied');
+  }
+
+  useEffect(() => {
+    if (!admin && session && session.user.role === 'admin') {
+      dispatch(fetchUserById({ userId: session.user.id, axiosWithAuth }));
+    }
+  }, [admin, dispatch, session, axiosWithAuth]);
+
   return (
     <div className="min-h-full">
       <AdminNavigation />
