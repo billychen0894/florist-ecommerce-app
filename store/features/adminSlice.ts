626c01d74e38e196853eecaa62891fec2d89c203
billychen0894 <billychen0894@gmail.com>
2023-10-06 21:26:13 -0700

    add: adminSlice for admin state management

---
+++ store/features/adminSlice.ts
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { User } from '@lib/types/api';
+import { Order } from '@prisma/client';
+import { AxiosInstance } from 'axios';
+import { admin } from '@lib/api/admin';
+
+// import type { PayloadAction} from "@reduxjs/toolkit";
+
+export interface AdminState {
+  accountUsers: User[];
+  orders: Order[];
+}
+
+const initialState: AdminState = {
+  accountUsers: [],
+  orders: [],
+};
+
+export const fetchAccountUsers = createAsyncThunk(
+  'admin/fetchAccountUsers',
+  async (axiosWithAuth: AxiosInstance, thunkAPI) => {
+    const response = await admin.getAllUsers(axiosWithAuth);
+    return response.data.data as User[];
+  }
+);
+
+const adminSlice = createSlice({
+  name: 'admin',
+  initialState,
+  reducers: {
+    // Add reducers here
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAccountUsers.fulfilled, (state, action) => {
+      state.accountUsers = action.payload;
+    });
+  },
+});
+
+export const {
+  /* Add action creators here */
+} = adminSlice.actions;
+
+export default adminSlice.reducer;
