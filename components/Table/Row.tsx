f04f60cfb503f7e3fdc1bf027b569a2a393ce9a3
billychen0894 <billychen0894@gmail.com>
2023-10-08 21:56:14 -0700

    update: Row component to be more flexible on rendering conditions

--- components/Table/Row.tsx
+++ components/Table/Row.tsx
@@ -1,19 +1,36 @@
 import { formatCurrency } from '@lib/formatCurrency';
-import { $Enums } from '@prisma/client';
+import { $Enums, Order } from '@prisma/client';
 import Link from 'next/link';
 import Stripe from 'stripe';
 import RowData from './RowData';
 
 interface RowProps {
   rowIndex: number;
-  invoice: Stripe.Invoice & { orderStatus: $Enums.OrderStatus };
+  invoice:
+    | (Stripe.Invoice & { orderStatus: $Enums.OrderStatus })
+    | (Order & { paymentStatus: $Enums.PaymentStatus });
   invoicesLength: number;
 }
 export default function Row({ rowIndex, invoice, invoicesLength }: RowProps) {
+  const invoiceNumber =
+    'number' in invoice ? invoice.number : invoice.orderNumber;
+  const paymentStatus =
+    'status' in invoice
+      ? invoice.status
+      : 'paymentStatus' in invoice
+      ? invoice.paymentStatus
+      : 'Error';
+  const invoiceUrl =
+    'hosted_invoice_url' in invoice
+      ? invoice.hosted_invoice_url
+      : 'orderNumber' in invoice
+      ? `/admin/orders/${invoice.orderNumber}`
+      : '#';
+
   return (
     <tr>
       <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
-        {invoice.number}
+        {invoiceNumber}
       </RowData>
       <RowData
         rowIndex={rowIndex}
@@ -40,16 +57,13 @@ export default function Row({ rowIndex, invoice, invoicesLength }: RowProps) {
       </RowData>
       <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
         <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-          {invoice.status === 'paid' ? 'Paid' : 'Unpaid'}
+          {paymentStatus?.charAt(0).toUpperCase()! +
+            paymentStatus?.slice(1).toLowerCase()!}
         </span>
       </RowData>
       <RowData rowIndex={rowIndex} invoiceLength={invoicesLength}>
         <Link
-          href={
-            typeof invoice.hosted_invoice_url === 'string'
-              ? invoice.hosted_invoice_url
-              : '#'
-          }
+          href={invoiceUrl as string}
           className="text-secondary-500 hover:text-secondary-300"
         >
           View invoice<span className="sr-only">, {''}</span>
