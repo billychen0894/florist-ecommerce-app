514d7b28b80efc3d200dd6688578b43c30520a56
billychen0894 <billychen0894@gmail.com>
2023-10-10 01:29:42 -0700

    add: formatTime helpers

---
+++ lib/formatTime.ts
@@ -0,0 +1,32 @@
+export const formatDateYYYYMMDD = (timestamp: string) => {
+  const date = new Date(Number(timestamp) * 1000);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate() + 1).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
+
+export const formatDateFullMonth = (timestamp: string) => {
+  const date = new Date(Number(timestamp) * 1000);
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const month = monthNames[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+};
