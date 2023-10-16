6ce01b09a6d1481789da0b0d9c344410c6f2962e
billychen0894 <billychen0894@gmail.com>
2023-10-15 19:19:56 -0700

    update: pass down categories object to AdminProductDetailsForm

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,7 @@
 import { fetchProducts } from '@actions/fetch-products';
 import AdminProductList from '@components/Admin/AdminProductList';
 import AdminProductDetailsForm from '@components/Admin/AdminProductDetailsForm';
+import { fetchCategories } from '@actions/fetch-categories';
 
 export default async function Products({
   searchParams,
@@ -18,11 +19,13 @@ export default async function Products({
     keyword
   );
 
+  const categories = await fetchCategories();
+
   return (
     <div className="mx-auto max-w-7xl py-8 sm:px-4 lg:px-12">
       <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
         <AdminProductList products={productsResult} keyword={keyword} />
-        <AdminProductDetailsForm />
+        <AdminProductDetailsForm categories={categories} />
       </div>
     </div>
   );
