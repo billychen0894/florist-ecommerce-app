bd7aadf346e46d2dc26060296bdf827649c3e3f7
billychen0894 <billychen0894@gmail.com>
2023-10-19 00:05:09 -0700

    add: SlideOver ui component

---
+++ components/ui/SlideOver.tsx
@@ -0,0 +1,60 @@
+import { Dialog, Transition } from '@node_modules/@headlessui/react';
+import { Dispatch, Fragment, SetStateAction } from 'react';
+import { cn } from '@lib/classNames';
+
+type SlideOverProps = {
+  children: React.ReactNode;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  className?: string;
+};
+
+export default function SlideOver({
+  children,
+  open,
+  setOpen,
+  className,
+}: SlideOverProps) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className={cn('relative z-30', className)}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-in-out duration-700"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in-out duration-700"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+        <div className="fixed inset-0">
+          <div className="fixed inset-0">
+            <div className="absolute inset-0 ">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16 overflow-scroll">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen h-screen max-w-2xl">
+                    {children}
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
