6e2c16605374db8073ec143aab2873766175468b
billychen0894 <billychen0894@gmail.com>
2023-10-22 00:14:34 -0700

    add: React Query setup

--- components/Providers/QueryProvider.tsx
+++ components/Providers/QueryProvider.tsx
@@ -1,16 +1,14 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
-
-const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function QueryProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  const [client] = React.useState(new QueryClient());
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
