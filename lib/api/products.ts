20fc17b5056c07c5d28e1d381d0e5df37ee84c89
billychen0894 <billychen0894@gmail.com>
2023-10-30 22:41:51 -0700

    update: remove react cache to api route methods

--- lib/api/products.ts
+++ lib/api/products.ts
@@ -1,44 +1,39 @@
 import axios from '@lib/axios';
 import { ApiResponse, TProduct } from '@lib/types/api';
 import { AxiosResponse } from 'axios';
-import { cache } from 'react';
 
-export const revalidate = 3600;
+const getAllProducts = async (
+  page?: number,
+  limit?: number,
+  sort?: 'popular' | 'newest' | 'price-high-to-low' | 'price-low-to-high',
+  filters?: string | string[] | undefined,
+  search?: string | undefined
+) => {
+  const params = new URLSearchParams();
+  page ? params.append('page', page.toString()) : undefined;
+  limit ? params.append('limit', limit.toString()) : undefined;
+  sort ? params.append('sort', sort) : undefined;
+  filters && Array.isArray(filters) && filters.length > 0
+    ? filters.forEach((filter) => params.append('filter', filter))
+    : typeof filters === 'string' && filters
+    ? params.append('filter', filters)
+    : undefined;
+  search ? params.append('keyword', search) : undefined;
 
-const getAllProducts = cache(
-  async (
-    page?: number,
-    limit?: number,
-    sort?: 'popular' | 'newest' | 'price-high-to-low' | 'price-low-to-high',
-    filters?: string | string[] | undefined,
-    search?: string | undefined
-  ) => {
-    const params = new URLSearchParams();
-    page ? params.append('page', page.toString()) : undefined;
-    limit ? params.append('limit', limit.toString()) : undefined;
-    sort ? params.append('sort', sort) : undefined;
-    filters && Array.isArray(filters) && filters.length > 0
-      ? filters.forEach((filter) => params.append('filter', filter))
-      : typeof filters === 'string' && filters
-      ? params.append('filter', filters)
-      : undefined;
-    search ? params.append('keyword', search) : undefined;
+  const response = (await axios.get(`/api/products`, {
+    params: params,
+  })) as AxiosResponse<ApiResponse<TProduct[]>>;
 
-    const response = (await axios.get(`/api/products`, {
-      params: params,
-    })) as AxiosResponse<ApiResponse<TProduct[]>>;
+  return response;
+};
 
-    return response;
-  }
-);
-
-const getProductById = cache(async (productId: string) => {
+const getProductById = async (productId: string) => {
   const response = (await axios.get(
     `/api/products/${productId}`
   )) as AxiosResponse<ApiResponse<TProduct>>;
 
   return response;
-});
+};
 
 export const products = {
   getAllProducts,
