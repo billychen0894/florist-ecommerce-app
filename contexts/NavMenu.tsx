8b9b1958a9d562ed16d6b431d8204979b4e65651
billychen0894 <billychen0894@gmail.com>
2023-11-03 00:13:15 -0700

    update: NavMenuContext remove searchWindowOpen state and setter

--- contexts/NavMenu.tsx
+++ contexts/NavMenu.tsx
@@ -11,15 +11,11 @@ import {
 interface NavMenuContextType {
   isMobileMenuOpen: boolean;
   setIsMobileMenuOpen: Dispatch<SetStateAction<boolean>>;
-  isSearchWindowOpen: boolean;
-  setIsSearchWindowOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 export const NavMenuContext = createContext<NavMenuContextType>({
   isMobileMenuOpen: false,
   setIsMobileMenuOpen: () => null,
-  isSearchWindowOpen: false,
-  setIsSearchWindowOpen: () => null,
 } as NavMenuContextType);
 
 export function NavMenuContextProvider({
@@ -28,16 +24,13 @@ export function NavMenuContextProvider({
   children: React.ReactNode;
 }) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
-  const [isSearchWindowOpen, setIsSearchWindowOpen] = useState<boolean>(false);
 
   const navMenuContext = useMemo(
     () => ({
       isMobileMenuOpen,
       setIsMobileMenuOpen,
-      isSearchWindowOpen,
-      setIsSearchWindowOpen,
     }),
-    [isSearchWindowOpen, isMobileMenuOpen]
+    [isMobileMenuOpen]
   );
 
   return (
