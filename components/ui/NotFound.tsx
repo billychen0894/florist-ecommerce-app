f8c6e93c5bf308da4142adea60f799acc8bba9ca
billychen0894 <billychen0894@gmail.com>
2023-11-06 07:35:34 -0800

    update: use dynamic import on NotFound page

---
+++ components/ui/NotFound.tsx
@@ -0,0 +1,37 @@
+import Image from '@node_modules/next/image';
+import Link from '@node_modules/next/link';
+import { ArrowSmallLeftIcon } from '@node_modules/@heroicons/react/20/solid';
+
+export default function NotFound() {
+  return (
+    <main className="relative isolate min-h-full">
+      <Image
+        src="/images/cover4.jpg"
+        alt="Red flowers background image"
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+        fill
+        sizes="100vw"
+        quality={60}
+        priority
+      />
+      <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+        <p className="text-base font-semibold leading-8 text-white">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
+          Page not found
+        </h1>
+        <p className="mt-4 text-base text-white/95 sm:mt-6">
+          Sorry, we couldn&apos;t find the page you&apos;re looking for.
+        </p>
+        <Link
+          href="/"
+          className="text-sm font-semibold leading-7 text-white group"
+        >
+          <div className="mt-10 inline-flex justify-center items-center group-hover:text-secondary-500">
+            <ArrowSmallLeftIcon className="h-5 w-5 text-white group-hover:text-secondary-500" />
+            Back to home
+          </div>
+        </Link>
+      </div>
+    </main>
+  );
+}
