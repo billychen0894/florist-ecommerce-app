6bb3f36eeded5cfaaaeac693231ef59f12e3ef3b
billychen0894 <billychen0894@gmail.com>
2023-11-06 07:35:21 -0800

    update: use dynamic import for Error page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-import Image from '@node_modules/next/image';
-import Button from '@components/ui/Button';
+import dynamic from 'next/dynamic';
 
 export default function GlobalError({
   error,
@@ -11,39 +10,12 @@ export default function GlobalError({
   error: Error;
   reset: () => void;
 }) {
+  const Error = dynamic(() => import('@components/ui/Error'));
+
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
-  return (
-    <main className="relative isolate min-h-full">
-      <Image
-        src="/images/cover4.jpg"
-        alt="Red flowers background image"
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
-        width={500}
-        height={500}
-        priority
-      />
-      <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-        <p className="text-base font-semibold leading-8 text-white">Oh no...</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
-          Something Went Wrong
-        </h1>
-        <p className="mt-4 text-sm text-white/95 sm:mt-6">
-          Our team is actively working to resolve the issue.
-        </p>
-        <div className="flex justify-center items-center mt-10 gap-4">
-          <Button
-            type="button"
-            className="bg-secondary-500 hover:bg-secondary-400"
-            onClick={() => reset()}
-          >
-            Try again
-          </Button>
-        </div>
-      </div>
-    </main>
-  );
+  return <Error reset={reset} />;
 }
