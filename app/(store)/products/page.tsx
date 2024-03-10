83a6073ff918b2537ccda994e078dc2152472042
billychen0894 <billychen0894@gmail.com>
2024-03-10 13:31:50 -0700

    update: use static import for image instead of using Cloudinary
    utilize next.js built-in optimization on image for SSR instead of CSR

--- app/(store)/products/page.tsx
+++ app/(store)/products/page.tsx
@@ -2,33 +2,26 @@ import { Breadcrumb } from '@components/Breadcrumb';
 import { Filter, Sort } from '@components/Filter';
 import { ProductList } from '@components/Product';
 import { Pagination } from '@components/Pagination';
-import { generateBase64 } from '@actions/generateBase64';
 import BannerImage from '@components/Images/BannerImage';
 import { Suspense } from 'react';
 import PaginationSkeleton from '@components/Product/PaginationSkeleton';
 import ProductListSkeleton from '@components/Product/ProductListSkeleton';
-
-const bannerText =
-  'Drifting in a sea of flowers, I am lost in the fragrance and beauty.';
+import bannerImage from '@public/images/cover3.jpg';
 
 export default async function Products({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const bannerBase64Url = await generateBase64(
-    `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/a_hflip.vflip,c_scale,dpr_auto,h_1080,q_60,w_1920/a_90/v1699079061/vjuw8dkm6btwiuow82xa.webp`
-  );
-
   return (
     <div className="bg-white">
       <div>
         <Breadcrumb />
         <main className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <BannerImage
-            cloudImagePublicId="vjuw8dkm6btwiuow82xa"
-            base64Url={bannerBase64Url}
-            bannerText={bannerText}
+            bannerImage={bannerImage}
+            bannerImageAlt="Sea of flowers"
+            bannerText="Drifting in a sea of flowers, I am lost in the fragrance and beauty."
           />
 
           {/* Filters */}
