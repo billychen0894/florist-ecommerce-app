7c5ff7f4c2dde57839de04729721adfb58aa70b3
billychen0894 <billychen0894@gmail.com>
2024-03-23 16:56:56 -0700

    chore: move fetchProducts server action to rpoductsActions.ts

--- actions/productsActions.ts
+++ actions/productsActions.ts
@@ -1,7 +1,70 @@
 'use server';
 
+import { parseSearchParams } from '@lib/parseSearchParams';
 import { prisma } from '@lib/prisma';
-import { revalidatePath } from 'next/cache';
+
+export const fetchProducts = async (
+  page?: string,
+  limit?: number,
+  sort?: string,
+  keyword?: string,
+  category?: string | string[]
+) => {
+  const {
+    page: pageNumber,
+    limit: take,
+    sort: sortOption,
+    keyword: search,
+    category: categoryFilters,
+  } = parseSearchParams(page, limit, sort, keyword, category);
+
+  const skip = limit ? (pageNumber - 1) * limit : 0;
+  const popular = sortOption === 'popular' ? 'desc' : 'asc';
+  const newest = sortOption === 'newest' ? 'desc' : 'asc';
+  const priceDirection = sortOption === 'price-high-to-low' ? 'desc' : 'asc';
+
+  let queryFilters = {
+    categories: {},
+    name: {},
+  };
+  if (categoryFilters.length > 0) {
+    queryFilters.categories = {
+      some: {
+        name: {
+          in: categoryFilters,
+        },
+      },
+    };
+  }
+
+  if (search) {
+    queryFilters.name = {
+      search,
+    };
+  }
+
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        images: true,
+        categories: true,
+      },
+      skip,
+      take,
+      orderBy: [
+        { price: priceDirection },
+        { createdAt: newest },
+        { orderItems: { _count: popular } },
+      ],
+      where: queryFilters,
+    });
+
+    return products;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 export async function getProductById(productId: string) {
   try {
@@ -22,7 +85,6 @@ export async function getProductById(productId: string) {
       },
     });
 
-    revalidatePath('(store)/products/[product]', 'page');
     return product;
   } catch (error) {
     console.error(error);
