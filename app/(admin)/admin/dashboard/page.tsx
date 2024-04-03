d8b2b0bc92cda1ceccd98467e6c4d727f17e531b
billychen0894 <billychen0894@gmail.com>
2024-04-02 19:00:58 -0700

    update: admin dashboard utilizing server actions and streaming

--- app/(admin)/admin/dashboard/page.tsx
+++ app/(admin)/admin/dashboard/page.tsx
@@ -2,10 +2,10 @@ import { redirect } from '@node_modules/next/navigation';
 import StatCards from '@components/Admin/StatCards';
 import { getServerSession } from 'next-auth/next';
 import { options } from '@app/api/auth/[...nextauth]/options';
-import { fetchProducts } from '@actions/fetch-products';
 import { fetchCategories } from '@actions/fetch-categories';
-
-export const dynamic = 'force-dynamic';
+import { fetchProducts } from '@actions/productsActions';
+import { Suspense } from 'react';
+import Spinner from '@components/ui/Spinner';
 
 export default async function Dashboard() {
   const session = await getServerSession(options);
@@ -14,16 +14,21 @@ export default async function Dashboard() {
     redirect('/denied');
   }
 
-  const products = await fetchProducts(1, 0, 'newest', undefined, undefined);
+  const products = await fetchProducts('1', 0, 'newest', undefined, undefined);
   const categories = await fetchCategories();
 
   return (
     <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <div>
-        <h3 className="text-base font-semibold leading-6 text-gray-900">
-          Last 30 days
-        </h3>
-        <StatCards products={products} categories={categories} />
+        <Suspense
+          fallback={
+            <div className="flex justify-center items-center">
+              <Spinner className="text-secondary-500 h-12 w-12 my-12" />
+            </div>
+          }
+        >
+          <StatCards products={products} categories={categories} />
+        </Suspense>
       </div>
     </div>
   );
--- components/Admin/StatCards.tsx
+++ components/Admin/StatCards.tsx
@@ -1,38 +1,43 @@
-'use client';
-
-import { useAppSelector } from '@store/hooks';
 import StatCard from '@components/ui/StatCard';
 import { formatCurrency } from '@lib/formatCurrency';
-import { TProduct } from '@lib/types/api';
-import { Category } from '@prisma/client';
+import { Category, Order } from '@prisma/client';
+import { TProducts, UserWithoutPass } from '@lib/types/types';
+import { getAllUsers, getOrders } from '@actions/adminActions';
 
 type StatCardsProps = {
-  products: TProduct[];
-  categories: Category[];
+  products: TProducts;
+  categories: Category[] | null;
 };
 
-export default function StatCards({ products, categories }: StatCardsProps) {
-  const accountUsers = useAppSelector(
-    (state) => state.adminReducer.accountUsers
-  );
-  const orders = useAppSelector((state) => state.adminReducer.orders);
-  const totalRevenue = orders.reduce((pre, curr) => {
-    return pre + curr.total;
-  }, 0);
+export default async function StatCards({
+  products,
+  categories,
+}: StatCardsProps) {
+  const promises: [Promise<UserWithoutPass[] | null>, Promise<Order[] | null>] =
+    [getAllUsers(), getOrders()];
+  const [accountUsers, orders] = await Promise.all(promises);
+  const totalRevenue = orders
+    ? orders?.reduce((pre, curr) => {
+        return pre + curr.total;
+      }, 0)
+    : 0;
 
   return (
     <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
       <StatCard
         name="Total Account Users"
-        stat={accountUsers.length.toString()}
+        stat={accountUsers?.length.toString() || ''}
       />
-      <StatCard name="Total Orders" stat={orders.length.toString()} />
+      <StatCard name="Total Orders" stat={orders?.length.toString() || ''} />
       <StatCard
         name="Revenue"
         stat={formatCurrency(totalRevenue, 'en-CA', 'CAD')}
       />
-      <StatCard name="Total SKU" stat={products.length.toString()} />
-      <StatCard name="Total Categories" stat={categories.length.toString()} />
+      <StatCard name="Total SKU" stat={products?.length.toString() || ''} />
+      <StatCard
+        name="Total Categories"
+        stat={categories?.length.toString() || ''}
+      />
     </dl>
   );
 }
