34e1611af06ea7270f05d6c1657f70dc43eb0661
billychen0894 <billychen0894@gmail.com>
2024-04-15 20:13:34 -0700

    update: signInFormSchema password validation message

--- lib/schemaValidator.ts
+++ lib/schemaValidator.ts
@@ -209,7 +209,9 @@ export type SignUpFormSchema = z.infer<typeof signUpFormSchema>;
 
 export const signInFormSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
-  password: z.string().min(8, { message: 'Password is required' }),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters' }),
 });
 
 export type SignInFormSchema = z.infer<typeof signInFormSchema>;
