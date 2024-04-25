48233fe1ff311d47d421032792d94e2e2c51798e
billychen0894 <billychen0894@gmail.com>
2024-04-24 18:19:10 -0700

    update: formatDate helper

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,8 +1,8 @@
 export function formatDate(date: Date): string {
   // Format date to "yyyy-mm-dd"
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const year = date?.getFullYear();
+  const month = date?.getMonth() + 1;
+  const day = date?.getDate();
 
   return `${year}-${month < 10 ? '0' : ''}${month}-${
     day < 10 ? '0' : ''
