dde8591a093c715dc5c9121c05def5827296e25a
billychen0894 <billychen0894@gmail.com>
2024-04-27 19:18:55 -0700

    chore: install jest testing libraries

---
+++ jest.config.ts
@@ -0,0 +1,35 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  // clearMocks: true,
+  // Indicates whether the coverage information should be collected while executing the test
+  // collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  // coverageDirectory: 'coverage',
+  // Indicates which provider should be used to instrument code for coverage
+  // coverageProvider: 'v8',
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  // moduleNameMapper: {
+  //   '^@/(.*)$': '<rootDir>/$1',
+  // },
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+};
+
+export default createJestConfig(config);
