47d60aa7266e98e1fe581ad4656afca30e1f8764
billychen0894 <billychen0894@gmail.com>
2024-05-03 20:20:09 -0700

    add: userSignUp test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,31 @@
 import { defineConfig } from 'cypress';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        async clearTestUser() {
+          await prisma.user.delete({
+            where: {
+              email: 'test10@test.com',
+            },
+          });
+          return null;
+        },
+        async seedTestUser() {
+          await prisma.user.create({
+            data: {
+              name: 'test10 test10',
+              email: 'test10@test.com',
+              password: 'TestTest10',
+            },
+          });
+          return null;
+        },
+      });
     },
     baseUrl: 'http://localhost:3000',
   },
